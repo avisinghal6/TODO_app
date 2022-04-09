376b274cdbae70e589bba55948a6f6efddd3f081
avi singhal <97785770+avisinghal6@users.noreply.github.com>
2022-04-09 23:45:01 +0530

    added, deleted tasks from db

--- controllers/task_controller.js
+++ controllers/task_controller.js
@@ -1,3 +1,42 @@
+const Task= require('../models/tasks_schema');
+
+
+
 module.exports.addTask= function(req,res){
-    return res.send("done");
+    Task.create(req.body, function(err, task){
+        if(err){
+            console.log('error in creating task');
+            return;
+        }
+        
+        return res.redirect('/');
+    });
+    
+}
+
+module.exports.displayTask= function(req,res){
+    Task.find({}, function(err, tasks){
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        
+        return res.render('createtasks',{
+            tasks: tasks
+        });
+    })
+}
+
+module.exports.deleteTask= function(req,res){
+    let taskId= req.params.id;
+    Task.findByIdAndDelete(taskId, function(err,deletedTask){
+        if(err){
+            console.log(err);
+        }
+        else{
+            
+            return res.redirect('back');
+        }
+    })
 }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 const taskController = require('../controllers/task_controller');
 
-router.get('/',homeController.home); 
+router.get('/',taskController.displayTask); 
 router.post('/add-task', taskController.addTask);
+router.get('/delete-task/:id', taskController.deleteTask);
 module.exports= router;
